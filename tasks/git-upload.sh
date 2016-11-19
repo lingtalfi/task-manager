05738eb9bd0692867daeffebfd47cfde2555a86e
lingtalfi <lingtalfi@gmail.com>
2016-11-19 15:09:53 +0100

    add git-upload example task

---
+++ tasks/git-upload.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# use git workflow here to mirror your local website to the server: 
+# https://github.com/lingtalfi/server-notes/blob/master/doc/deploy-website.md
+
+# then snap and pp are some shortcuts that I use
+# https://github.com/lingtalfi/my-git-config
+
+cd "/pathto/my/app"
+git snap automatic commit
+git pp
+
+ 
